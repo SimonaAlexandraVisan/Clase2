32dd3ddae5cef293569bc6bebb48dd38eada5240
SimonaAlexandraVisan <122809090+SimonaAlexandraVisan@users.noreply.github.com>
2023-01-17 19:06:30 +0100

    Corrcción dimensiones y orden del array

--- EjercicioPasswd/src/Main.java
+++ EjercicioPasswd/src/Main.java
@@ -10,19 +10,19 @@ public class Main {
 		Password[] arr = new Password[dim];
 		System.out.println("Introduzca la longitud de la contrasenia: ");
 		Integer longitud = sc.nextInt();
-		for(int i=0; i<dim-1; i++) {
+		for(int i=0; i<dim; i++) {
 			arr[i] = new Password(longitud);
 		}
 		
 		Password[] arr_clon = new Password[dim];
-		for(int i=0; i<dim-1; i++) {
+		for(int i=0; i<dim; i++) {
 			arr_clon[i] = new Password(arr[i]);
 		}
 		
 		String aux = "";
-        for (int i = 0; i < dim-1; i++) {
-            for (int j = 1; j < dim-1; j++) {
-                if(arr_clon[j].getContrasenia().charAt(0) < arr_clon[j-1].getContrasenia().charAt(0)){
+        for (int i = 0; i < dim; i++) {
+            for (int j = 1; j < dim; j++) {
+                if(arr_clon[j].getContrasenia().toUpperCase().charAt(0) < arr_clon[j-1].getContrasenia().toUpperCase().charAt(0)){
                     aux = arr_clon[j].getContrasenia();
                     arr_clon[j].setContrasenia(arr_clon[j-1].getContrasenia());
                     arr_clon[j-1].setContrasenia(aux);
@@ -32,17 +32,17 @@ public class Main {
 
         
         System.out.println("Se muestra el contenido: ");
-        for(int i=0; i<dim-1; i++) {
+        for(int i=0; i<dim; i++) {
 			System.out.println(arr_clon[i].toString());
 		}
         
         Boolean[] esFuerte = new Boolean[dim];
-        for(int i=0; i<dim-1; i++) {
+        for(int i=0; i<dim; i++) {
 			esFuerte[i] = arr[i].esFuente();
 		}
 		
         System.out.println("Se muestra la contrasenia y si es fuerte: ");
-        for(int i=0; i<dim-1; i++) {
+        for(int i=0; i<dim; i++) {
 			System.out.println("Contrasenia: " + arr[i].toString() + " es fuerte: " + esFuerte[i]);
 		}
 	}
